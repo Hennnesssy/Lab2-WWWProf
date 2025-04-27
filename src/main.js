882f61d784ef29d7177460f04d8a26deda201399
Hennnesssy <ruslanshatkun@gmail.com>
2025-04-27 18:03:04 +0200

    refactor: comment out unused CSS import in main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './assets/main.css'
+//import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,9 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
